4e564a9d465f36c44e951e929d4e3bb76d8bfb27
Hivison do Nascimento Moura <hivison.moura@gmail.com>
2016-10-20 10:26:49 -0200

    Criação da entidade OrdemDeServico com as variáveis codigo e quantidade dos tipos String e Int respectivamente.

---
+++ ControleEstoque/src/Entidades/OrdemDeServico.java
@@ -0,0 +1,28 @@
+package Entidades;
+
+public class OrdemDeServico {
+	
+
+	private String codigo;
+	private int quantidade;
+	
+	/*
+	 * GETTERS AND SETTERS
+	 */
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+
+}
